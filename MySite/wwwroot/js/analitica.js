2a20a8efa3cadc7de5b879b1b969104058e9cc87
Kostya <gnil47311@gmail.com>
2023-11-27 14:56:48 +0700

    1

--- MySite/wwwroot/js/analitica.js
+++ MySite/wwwroot/js/analitica.js
@@ -1,13 +1,18 @@
 ﻿
 
-
-
-
 let table = new DataTable('#myTable');
-$("#myTable").DataTable();
-loadDT();
 
-
+const response = await fetch('https://countryapi.io/api/all/?apikey=q0wj7CJmrMvABF8ZpUWR5Wxd77jNqGlC5ynHScwt');
+var myJson = await response.json();
+$.each(myJson, function (index, v) {
+    table.row
+        .add([
+            v.name,
+            v.official_name,
+            v.region
+        ])
+        .draw(false);
+})
 
 
 
